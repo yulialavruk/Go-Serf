468c54169d796fafa03841a00c4537bb8fc73e58
Lavruk Yulia <lavruk_yulia@ukr.net>
2020-03-05 19:55:56 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="" />',
+    nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="img/arrows-right.svg" alt="" />',
+    asNavFor: '.slider-dots'
+  });
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+  })
+
+  
+});
